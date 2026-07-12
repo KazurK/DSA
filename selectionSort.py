e88e2d6040d311cfaca65e2d88a15daa16d79bf0
Jaden Foster <fosterj19@coventry.ac.uk>
2026-07-12 12:56:02 +0100

    Selection sort

---
+++ selectionSort.py
@@ -0,0 +1,15 @@
+def selectionSort():
+    sort = [64, 34, 25, 5, 22, 11, 90, 12]
+    n = len(sort)
+    for i in range(n-1):
+        minIndex = i
+        for j in range(i+1, n):
+            if sort[j] < sort[minIndex]:
+                minIndex = j
+        sort[i], sort[minIndex] = sort[minIndex], sort[i]
+
+
+    print("Sorted array:", sort)
+
+if __name__ == "__main__":
+    selectionSort()
