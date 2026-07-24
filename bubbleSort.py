2b79541670c241f63c1bff91fc6c8fcdbe77647b
Jaden Foster <fosterj19@coventry.ac.uk>
2026-07-24 14:30:00 +0100

    Bubble Sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,16 +1,13 @@
-def bubbleSort():
-    sort = [1, 3,45,422121212121212,66,0,0.0001]
-    n = len(sort)
-    for i in range(n-1): #Loop through sort
-        swapped = False
-        for x in range(n-i-1): #closing loop through sort
-            if sort[x] > sort[x+1]:
-                sort[x], sort[x+1] = sort[x+1], sort[x]
-                swapped = True
+
+sort = [64, 34, 25, 5, 22, 11, 90, 12]
+n = len(sort)
+for i in range(n-1): #Loop through sort
+    swapped = False
+    for x in range(n-i-1): #closing loop through sort
+        if sort[x] > sort[x+1]:
+            sort[x], sort[x+1] = sort[x+1], sort[x]
+            swapped = True
         if not swapped:
             break
 
-    print("Sorted array:", sort)
-
-if __name__ == "__main__":
-    bubbleSort()
+print("Sorted array:", sort)
